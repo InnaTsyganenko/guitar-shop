5a3cf6ada17a89c15345a4f30c1b17563b66b5c9
Nikulichka <poteryaevainna@gmail.ru>
2022-05-16 13:12:57 +0300

    Fix bug persistConfig

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -5,21 +5,12 @@ import CatalogScreen from '../catalog-screen/catalog-screen';
 import CartScreen from '../cart-screen/cart-screen';
 import ProductScreen from '../product-screen/product-screen';
 import NotFoundScreen from '../not-found-screen/not-found-screen';
-import LoadingScreen from '../loading-screen/loading-screen';
 import { useAppSelector } from '../../hooks';
-import { getStatusLoadedData } from '../../store/guitars-data/selectors';
 import { getCurrentPageCatalog, getPickedId } from '../../store/guitars-operations/selectors';
 
 function App(): JSX.Element {
-  const isDataLoaded = useAppSelector(getStatusLoadedData);
   const currentPageCatalog = useAppSelector(getCurrentPageCatalog);
   const pickedId = useAppSelector(getPickedId);
-
-  if (!isDataLoaded) {
-    return (
-      <LoadingScreen />
-    );
-  }
 
   return (
     <Routes>
